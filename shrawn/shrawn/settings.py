e86c20fd2d841e64ff62018d27d35fad4877b7d8
siddartha <siddarthasubedi1@gmail.com>
2026-07-25 16:36:29 +0545

    Update cors for frontend

--- shrawn/shrawn/settings.py
+++ shrawn/shrawn/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    # "https://your-frontend-name.vercel.app",
+    "https://shrawan-frontend-abc.onrender.com",
 ]
 
 
